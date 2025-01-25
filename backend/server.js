8376194ca680e58ab570456099c13ab2a2b27d50
NA980593 <NA980593@wcupa.edu>
2025-01-25 11:36:45 -0500

    add endpoint to get all farms

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ const app = express()
 
 app.use(express.json());
 
+app.get("/api/farms", async (req, res)=> {
+    try {
+        const farms = await Farm.find({});
+        res.status(200).json({success: true, data: farms});
+    } catch (error) {
+        console.log("Error fetching farms:", error.message);
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+});
+
 app.post("/api/farms", async (req,res)=> {
     const farm = req.body;
 
@@ -30,6 +40,7 @@ app.delete("/api/farms/:id", async (req,res)=> {
         await Farm.findByIdAndDelete(id);
         res.status(200).json({success: true, message: "Farm deleted."});
     } catch (error) {
+        console.log("Error deleting farm:",error.message);
         res.status(404).json({success: false, message: "Farm not found."});
     }
 });
