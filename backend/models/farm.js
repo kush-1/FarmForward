133980a28e7c4169152c5308c9b79425fd522caf
NA980593 <NA980593@wcupa.edu>
2025-01-25 08:29:26 -0500

    add required image and timestamps to farm.js

--- backend/models/farm.js
+++ backend/models/farm.js
@@ -13,6 +13,10 @@ const farmSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  image: {
+    type: String,
+    required: true
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User", // Relates to the User model
