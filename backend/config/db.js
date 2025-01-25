0b39158451fafe42bcd136cfe6c861b11f5170de
NA980593 <NA980593@wcupa.edu>
2025-01-25 04:53:45 -0500

    fix moving dotenv config broke it. moved it back to db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,10 @@
 import mongoose, { connect } from 'mongoose';
 
-const connectDB = async () => {
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
   try {
     await connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import {connectDB} from './config/db.js';
 
 const app = express()
 
 app.listen(3000, () => {
+    connectDB();
     console.log('Server is running on port 3000');
 });
