c921bec29bfce8f60c391178898c05168bc6d40e
NA980593 <NA980593@wcupa.edu>
2025-01-25 10:50:48 -0500

    add endpoint for creating new farms

--- backend/models/farm.js
+++ backend/models/farm.js
@@ -1,29 +1,31 @@
 import mongoose from 'mongoose';
 
-const farmSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const farmSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    // owner: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "User", // Relates to the User model
+    //   required: true,
+    // },
   },
-  location: {
-    type: String,
-    required: true,
-  },
-  size: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Relates to the User model
-    required: true,
-  },
-  timestamps: true
-});
+  {timestamps: true}
+);
 
 const Farm = mongoose.model('Farm', farmSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,28 @@
 import express from 'express';
 import {connectDB} from './config/db.js';
+import Farm from "./models/farm.js";
 
 const app = express()
+
+app.use(express.json());
+
+app.post("/api/farms", async (req,res)=> {
+    const farm = req.body;
+
+    if(!farm.name || !farm.location || !farm.size || !farm.image){
+        return res.status(400).json({success:false, message: "Please provide all the required fields."});
+    }
+
+    const newFarm = new Farm(farm);
+
+    try {
+        await newFarm.save();
+        res.status(201).json({success: true, data: newFarm});
+    } catch (error) {
+        console.error("Error creating the farm:",error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+});
 
 app.listen(3000, () => {
     connectDB();
