f7beccd5527d18d6fe47ff7873b4d4e4c2799896
NA980593 <NA980593@wcupa.edu>
2025-01-25 04:57:34 -0500

    refactor remove deprecated options

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Database connection error:', error);
