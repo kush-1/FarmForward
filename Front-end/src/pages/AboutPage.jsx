2db628e265f729c63975fa535b097a05f5c6d9d1
jp1014506 <121205145+jp1014506@users.noreply.github.com>
2025-01-25 21:55:53 -0500

    Mission Statement, update 2

--- Front-end/src/pages/AboutPage.jsx
+++ Front-end/src/pages/AboutPage.jsx
@@ -2,7 +2,13 @@ import { Container, Flex, Text } from "@chakra-ui/react"
 
 const AboutPage = () => {
     return (
-        <div>Farm Forward is a nonprofit organization committed to securing the future of agriculture by connecting the next generation of farmers with retiring operators and landowners. Through our innovative database and community-driven approach, we aim to preserve agricultural lands, sustain essential farming knowledge, and ensure the infrastructure necessary for thriving agricultural systems. Our mission is to build relationships that safeguard the transfer of farmland and foster sustainable food systems vital to the health and well-being of communities worldwide. 
+        <div>Farm Forward is a nonprofit organization committed to securing the future<br> 
+	of agriculture by connecting the next generation of farmers with retiring operators<br> 
+	and landowners. Through our innovative database and community-driven approach, we aim<br> 
+	to preserve agricultural lands, sustain essential farming knowledge, and ensure the<br> 
+	infrastructure necessary for thriving agricultural systems. Our mission is to build<br> 
+	relationships that safeguard the transfer of farmland and foster sustainable food<br>
+	 systems vital to the health and well-being of communities worldwide. 
         </div>
     );
 };
