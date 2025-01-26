a55c2592a2fd61646b021e18a99ba8cedad0cbe5
NA980593 <NA980593@wcupa.edu>
2025-01-26 07:21:13 -0500

    hosting setup stuff

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,10 @@ app.use(express.json());
 app.use("/api/farms",farmRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/Front-end/dist")));
 
     app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+        res.sendFile(path.resolve(__dirname,"Front-end","dist","index.html"));
     })
 }
 
